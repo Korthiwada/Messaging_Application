1c9f593916e3ccb23a7dfb584978747f6b973405
Korthiwada <korthiwadarahul@gmail.com>
2018-03-25 19:14:45 +0530

    Added Components in inbox and Sent Mail

--- App.jsx
+++ App.jsx
@@ -1,44 +1,128 @@
+var Recieved_Mail_From = ["Rahul" , "Ganesh"];
+var Recieved_Mail_Subject = ["Good Morning" , "Bless You"];
+var Recieved_Mail_Body = ["Hi I am there .Where were you?" , "Bless You My Child"];
 
-var RecievedMailFrom = ["Rahul" , "Ganesh"];
-var RecievedMailSubject = ["Good Morning" , "Bless You"];
-var RecievedMailBody = ["Hi I am there .Where were you?" , "Bless You My Child"];
-var k =2;
+var Sent_Mail_To = ["Ra" , "Ga"];
+var Sent_Mail_Subject = ["Good Morning" , "Bless You"];
+var Sent_Mail_Body = ["Hi I am there .Where were you?" , "Thank You God"];
+var k =0;
 
-/*var Message;
-Message = React.createClass({
+var SMessage;
+SMessage = React.createClass({
+    getInitialState : function () {
+        return{
+            state : false
+        }
+    },
+
+    BodyDisplay : function () {
+        this.setState(
+            {state : true }
+        )
+    },
+
+    BodyNDisplay : function () {
+        this.setState(
+            {state : false }
+        )
+    },
+
     render : function () {
-        return(
-            <li>{RecievedMailFrom[this.props.id]}
-            --{RecievedMailSubject[this.props.id]}</li>
-        );
+        switch (this.state.state) {
+            case false :
+                return(
+                    <form onClick={this.BodyDisplay}>{Sent_Mail_To[this.props.id]}
+                        --{Sent_Mail_Subject[this.props.id]}</form>
+                );
+                break;
+            case true :
+                return(
+                    <form onClick={this.BodyNDisplay}><li>{Sent_Mail_To[this.props.id]}
+                        --{Sent_Mail_Subject[this.props.id]}</li> <br/> <p>{Sent_Mail_Body[this.props.id]}</p></form>
+                );
+                break;
+        }
     }
-});*/
+});
+
+
+
+
+var IMessage;
+IMessage = React.createClass({
+    getInitialState : function () {
+        return{
+            stat : false
+        }
+    },
+
+    BodyDisplay : function () {
+        this.setState(
+            {stat : true }
+        )
+    },
+
+    BodyNDisplay : function () {
+        this.setState(
+            {stat : false }
+        )
+    },
+
+    render : function () {
+        switch (this.state.stat) {
+            case false :
+                return(
+            <li onClick={this.BodyDisplay}>{Recieved_Mail_From[this.props.id]}
+            --{Recieved_Mail_Subject[this.props.id]}</li>
+        );
+        break;
+            case true :
+                return(
+                <ul onClick={this.BodyNDisplay}><li>{Recieved_Mail_From[this.props.id]}
+                    --{Recieved_Mail_Subject[this.props.id]}</li> <br/> <p>{Recieved_Mail_Body[this.props.id]}</p></ul>
+        );
+                break;
+            }
+    }
+});
 
 var Inbox;
 Inbox = React.createClass({
     render: function () {
-        return(
-            <div>
-                <ul>
-                    <h2>Messages</h2>
-                    /*<Message id=0/>*/
-                        {/*<li>{RecievedMailFrom[1]}*/}
-                            {/*--{RecievedMailSubject[1]}</li>*/}
+            return (
+                <div>
+                    <ul>
+                        <h2>Messages</h2>
+                        <IMessage id="0"/>
+                        <IMessage id="1"/>
                     </ul>
                 </div>
             );
-    }
+        }
 });
 
 
 var Compose;
 Compose = React.createClass({
-    UpdateSentMail: function () {
+    getInitialState : function () {
+        return{
+            s : 0
+        }
+    },
+
+    Acknowledge : function () {
         this.setState(
-            {status : 1 }
+            {s : 1 }
+        )
+    },
+
+    MessageSent : function () {
+        this.setState(
+            {s : 0 }
         )
     },
     render: function (){
+        if(this.state.s==0){
         return (
 
             <div>
@@ -54,11 +138,21 @@ Compose = React.createClass({
                 <input type="text">
                 </input>
                 <br></br>
-
-                <button onClick={this.UpdateSentMail}>Send</button>
+                <button onClick={this.Acknowledge}>Send</button>
             </div>
-
         );
+        }
+        else
+        {
+          return(
+              <div>
+                  <h4>Message Successfully Sent</h4>
+                  {this.MessageSent};
+                  <Inbox/>
+                  </div>
+          );
+        }
+
     }
 });
 
@@ -68,7 +162,13 @@ Sent = React.createClass({
     render: function (){
         return (
 
-            <p>Hi Sent</p>
+            <div>
+                <ul>
+                    <h2>Messages</h2>
+                    <SMessage id={k%2} />
+                    <SMessage id={(k+1)%2}/>
+                </ul>
+            </div>
 
         );
     }
