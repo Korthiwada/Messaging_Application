43d75ad7c5753f04e04e856729f008eebb52a4d2
Korthiwada <korthiwadarahul@gmail.com>
2018-03-25 13:16:35 +0530

    Basic Functionalities of Side_Navigation Bar

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+var MyComponent = React.createClass({
+    render : function (){
+        return(
+        <h1> "Hello World"</h1>
+    );
+    }
+
+});
+
+ReactDOM.render(
+    <WallContainer/>,
+    document.getElementById('container')
+);
